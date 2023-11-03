b9f3a25a838c3cd9b71004e02a7cfee1a04a68b3
Gakuruken <149758032+Gakuruken@users.noreply.github.com>
2023-11-03 08:58:31 +0300

    Add files via upload

---
+++ javascript ken/js/index.js
@@ -0,0 +1,8 @@
+function display(){
+//alert("Welcome to our System");
+document.getElementById("heading").innerHTML="This is Javascript";
+}
+function hide(){
+    document.getElementById("heading").innerHTML="";
+}
+
